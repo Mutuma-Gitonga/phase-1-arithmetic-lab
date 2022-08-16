d2db74bc926f9c57cd3910cd95b6bf909c0cb613
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-16 09:44:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+
+// Create a variable called random that will generate a random integer greater than 0.
+const random = Math.ceil(Math.random());
+
+
+//Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+const num3 = 25;
+const num4 = 7;
+
+const mod = num3 % num4;
+
+
+// 
+const max = Math.max(2,4,5,8,20);
